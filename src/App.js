6154e3290cc5d7fcea37246b3da062cf8c7a167f
Blessedadudu <adududuke@gmail.com>
2020-05-14 12:09:23 +0100

    hooks setup

--- src/App.js
+++ src/App.js
@@ -1,24 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
 
-function App() {
+const App = () => {
+  const [posts, setPosts] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(10);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      setLoading(true);
+      try {
+         const res = await axios.get('https://jso/nplaceholder.typicode.com/posts');
+      setPosts(res.data);
+      setLoading(false);
+      console.error(res);
+      } catch (error) {
+        console.error(error);
+      }
+     
+    };
+
+    fetchPosts();
+  }, []);
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='container mt-5'>
+      <h1 className='text-primary mb-3'>My Blog</h1>
+      {/* <Posts posts={currentPosts} loading={loading} /> */}
+      {/* <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={posts.length}
+        paginate={paginate}
+      /> */}
     </div>
   );
 }
